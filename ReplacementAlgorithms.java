42caba88f764378c0d39c0302ff87d4c40bf481d
Samuel Zrna <47229887+samuelzrna@users.noreply.github.com>
2019-05-08 20:50:06 -0500

    Create ReplacementAlgorithms.java

---
+++ ReplacementAlgorithms.java
@@ -0,0 +1,164 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ReplacementAlgorithms {
+
+	private static Scanner scnr;
+
+	public static void main(String[] args) {
+		
+		if (args.length > 0) {
+			
+			List<Integer> pages = new ArrayList<Integer>();
+			
+			try
+			{
+				File file = new File(args[0]);
+				scnr = new Scanner(file);
+				while(scnr.hasNextLine()){
+					String line =  scnr.nextLine();
+					String data = "";
+					for (int i = 0; i < line.length(); i++){
+						char c = line.charAt(i);
+						if(c == ' ') {
+							pages.add(Integer.parseInt(data));
+							data = "";
+						} else
+							data += c;
+					}
+				}
+				 
+			} catch (IOException e) {
+				System.out.println("Error accessing input file!");
+			}
+			 
+			for(int i = 1; i <= Integer.parseInt(args[1]); i++) {
+				int faults = LRU(pages, i, 0);
+				System.out.println(i + "\t" + faults);
+				
+				faults = RandomReplacement(pages, i, 0);
+				System.out.println(i + "\t" + faults);
+				
+				faults = FIFO(pages, i, 0);
+				System.out.println(i + "\t" + faults);
+				
+				faults = Optimal(pages, i, 0);
+				System.out.println(i + "\t" + faults);
+			}
+		}
+	}
+	
+	public static int LRU(List<Integer> pages, int frameCount, int faultCount) {
+		Queue<Integer> frames = new LinkedList<Integer>();
+		boolean faultOccurs = true;
+		
+		for(int i = 0; i < pages.size(); i++) {
+			if(i < frameCount) {
+				frames.add(pages.get(i));
+				faultCount++;
+			} else {
+				for(Integer frameContents : frames)
+					if(frameContents == pages.get(i)) faultOccurs = false;
+				
+				if(faultOccurs) faultCount++;
+				
+				frames.remove();
+				frames.add(pages.get(i));
+				faultOccurs = true;
+			}
+		}
+		return faultCount;
+	}
+	
+	public static int RandomReplacement(List<Integer> pages, int frameCount, int faultCount) {
+		int[] frames = new int[frameCount];
+		boolean faultOccurs = true;
+		Random rand = new Random();
+		
+		for(int i = 0; i < pages.size(); i++) {
+			if(i < frameCount) {
+				frames[i] = pages.get(i);
+				faultCount++;
+			} else {
+				for(Integer frameContents : frames)
+					if(frameContents == pages.get(i)) faultOccurs = false;
+				
+				if(faultOccurs) faultCount++;
+				
+				frames[rand.nextInt(frameCount)] = pages.get(i);
+				faultOccurs = true;
+			}
+		}
+		return faultCount;
+	}
+	
+	public static int FIFO(List<Integer> pages, int frameCount, int faultCount) {
+		Queue<Integer> frames = new LinkedList<Integer>();
+		boolean faultOccurs = true;
+		
+		for(int i = 0; i < pages.size(); i++) {
+			if(i < frameCount) {
+				frames.add(pages.get(i));
+				faultCount++;
+			} else {
+				for(Integer frameContents : frames)
+					if(frameContents == pages.get(i)) faultOccurs = false;
+				
+				if(faultOccurs) {
+					faultCount++;
+					frames.remove();
+					frames.add(pages.get(i));
+				}
+				faultOccurs = true;
+			}
+		}
+		return faultCount;
+	}
+	
+	public static int Optimal(List<Integer> pages, int frameCount, int faultCount) {
+		int[] frames = new int[frameCount];
+		boolean faultOccurs = true;
+		
+		for(int i = 0; i < pages.size(); i++) {
+			if(i < frameCount) {
+				frames[i] = pages.get(i);
+				faultCount++;
+			} else {
+				for(Integer frameContents : frames)
+					if(frameContents == pages.get(i)) faultOccurs = false;
+				
+				if(faultOccurs) {
+					faultCount++;
+					int optimalFound = 0;
+					int j = i;
+					int currentOptimal = 0;
+					while(optimalFound < frameCount && j < pages.size()) {
+						for (int k = 0; k < frames.length; k++) {
+							if (pages.get(j) == frames[k]) {
+								optimalFound++;
+								currentOptimal = frames[k];
+							}
+						}
+						j++;
+					}
+					for(int k = 0; k < frames.length; k++)
+						if(currentOptimal == frames[k])
+							frames[k] = pages.get(i);
+				}
+				faultOccurs = true;
+			}
+		}
+		return faultCount;
+	}
+}
